5ec5659fa0fa4bf9b0043b8287a37810977e4836
Jordan Marshall <60512350+PlantEcology@users.noreply.github.com>
2023-08-17 15:49:43 -0400

    Add files via upload

--- R/funredun.R
+++ R/funredun.R
@@ -1,5 +1,5 @@
 #' Functional Redundancy
-#' 
+#'
 #' The function calculates functional redundancy as Simpson's D minus dissimilarity between species weighted by the proportions of the species.
 #'
 #' @param spDat Data frame with rows as sites, columns as species, and elements as counts
@@ -12,30 +12,30 @@
 
 funredun=function(spDat,funDat,method='gower',redund=TRUE,funDiv=FALSE){
   #sets progress bar
-  pb=txtProgressBar(min=0,max=nrow(spDat),style=3,width=50,char="=")
-  
+  pb=utils::txtProgressBar(min=0,max=nrow(spDat),style=3,width=50,char="=")
+
   #calculates distance between species based on functional traits
   spDist=vegan::vegdist(funDat,method=method,upper=T)
-  
+
   #creates a vector of Simpson's diversity for the community
   D=as.vector(vegan::diversity(spDat,index='simpson'))
-  
+
   #creates empty data frame P with same columns as input community data
   P=spDat[0,]
-  
+
   #loop to calculate proportion of each species at each site
   for(i in 1:nrow(spDat)){
     for(j in 1:ncol(spDat)){
       P[i,j]=spDat[i,j]/sum(spDat[i,])
     }
   }
-  
+
   #empty vectors of Q and FR
   Q=vector()
   FR=vector()
   Q=rep(0,nrow(P))
   FR=rep(0,nrow(P))
-  
+
   #loop to calculate Q as product of dissimilarity between species i and j, proportion of species i, proportion of species j
   for(i in 1:nrow(P)){
     for(j in 1:ncol(P)){
@@ -43,20 +43,20 @@ funredun=function(spDat,funDat,method='gower',redund=TRUE,funDiv=FALSE){
         Q[i]=Q[i]+(usedist::dist_get(spDist,names(P)[j],names(P)[k])*P[i,j]*P[i,k])
       }
     }
-    
+
     #calculates function redundance as D-Q or functional uniqueness Q/D
     if (redund==TRUE) {
       FR[i]=(D[i]-Q[i])
     } else {
       if (D[i]==0) {
-        FR[i]==NA
+        FR[i]=NA
       } else {
       FR[i]=Q[i]/D[i]
       }
     }
-    setTxtProgressBar(pb,i)
+    utils::setTxtProgressBar(pb,i)
   }
- 
+
   #builds output
   if (funDiv==FALSE) {
     FRoutput=data.frame(cbind(FR))
