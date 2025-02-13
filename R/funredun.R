d56b673824327ad5a3f8a4ad5c3069ad35680201
Jordan Marshall <60512350+PlantEcology@users.noreply.github.com>
2025-02-13 09:13:26 -0500

    Add files via upload

--- R/funredun.R
+++ R/funredun.R
@@ -15,7 +15,7 @@
 
 funredun<-function(spDat,funDat,method='gower',redund=TRUE,funDiv=FALSE){
   #sets progress bar
-  pb<-utils::txtProgressBar(min=0,max=nrow(spDat),style=3,width=50,char="▓")
+  pb<-utils::txtProgressBar(min=0,max=nrow(spDat),style=3,width=50,char="=")
 
   #calculates distance between species based on functional traits
   spDist<-vegan::vegdist(funDat,method=method,upper=T,na.rm=T)
